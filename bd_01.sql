3e69a1299ebe08ca20f48a4e2d5f7681c92fd5db
MeryAna1 <anamerysilvacordovil@gmail.com>
2026-04-16 11:21:53 -0300

    Add files via upload

---
+++ bd_01.sql
@@ -0,0 +1,9 @@
+USE projeto_1;
+
+SELECT COUNT(*) AS total_alunos FROM tbProfessor;
+
+SELECT AVG(matricula_professor) AS media_matricula_prof
+FROM tbProfessor;
+
+sELECT MAX(codigo_disciplina) FROM tbDisciplina;
+
