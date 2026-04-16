ca96c1e3f1a080537c3292a31656022edc1baf28
MeryAna1 <anamerysilvacordovil@gmail.com>
2026-04-16 11:27:53 -0300

    Delete bd_01.sql

--- bd_01.sql
+++
@@ -1,9 +0,0 @@
-USE projeto_1;
-
-SELECT COUNT(*) AS total_alunos FROM tbProfessor;
-
-SELECT AVG(matricula_professor) AS media_matricula_prof
-FROM tbProfessor;
-
-sELECT MAX(codigo_disciplina) FROM tbDisciplina;
-
